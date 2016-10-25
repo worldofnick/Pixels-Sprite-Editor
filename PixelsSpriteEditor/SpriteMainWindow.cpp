08ccff1ea55587052df6604cd27f41647bb4b632
Tyler Dailey <tkdailey11@gmail.com>
2016-10-25 15:56:24 -0600

    Added some stub slots, also added a color picker that sets the color of the member variable QPen.

--- PixelsSpriteEditor/SpriteMainWindow.cpp
+++ PixelsSpriteEditor/SpriteMainWindow.cpp
@@ -1,3 +1,4 @@
+#include <QColorDialog>
 #include "SpriteMainWindow.h"
 #include "ui_SpriteMainWindow.h"
 
@@ -6,9 +7,62 @@ SpriteMainWindow::SpriteMainWindow(QWidget *parent) :
     ui(new Ui::SpriteMainWindow)
 {
     ui->setupUi(this);
+
+    //penColor is recorded so that when a color picker is selected,
+    //the beginning color will be the current penColor.
+    penColor = Qt:: blue;
+    pen.setColor(penColor);
 }
 
 SpriteMainWindow::~SpriteMainWindow()
 {
     delete ui;
+}
+
+
+//Slots*********************************************
+
+void SpriteMainWindow::on_colorPickButton_clicked()
+{
+    pen.setColor(QColorDialog::getColor(penColor));
+}
+
+void SpriteMainWindow::on_stampTool_clicked()
+{
+
+}
+
+void SpriteMainWindow::on_ellipseTool_clicked()
+{
+
+}
+
+void SpriteMainWindow::on_rectangleTool_clicked()
+{
+
+}
+
+void SpriteMainWindow::on_lineTool_clicked()
+{
+
+}
+
+void SpriteMainWindow::on_eraserTool_clicked()
+{
+
+}
+
+void SpriteMainWindow::on_penTool_clicked()
+{
+
+}
+
+void SpriteMainWindow::on_addFrameButton_clicked()
+{
+
+}
+
+void SpriteMainWindow::on_fpsSlider_valueChanged(int value)
+{
+
 }
--- PixelsSpriteEditor/SpriteMainWindow.h
+++ PixelsSpriteEditor/SpriteMainWindow.h
@@ -2,6 +2,7 @@
 #define SPRITEMAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPen>
 
 namespace Ui {
 class SpriteMainWindow;
@@ -15,8 +16,29 @@ public:
     explicit SpriteMainWindow(QWidget *parent = 0);
     ~SpriteMainWindow();
 
+private slots:
+    void on_colorPickButton_clicked();
+
+    void on_stampTool_clicked();
+
+    void on_ellipseTool_clicked();
+
+    void on_rectangleTool_clicked();
+
+    void on_lineTool_clicked();
+
+    void on_eraserTool_clicked();
+
+    void on_penTool_clicked();
+
+    void on_addFrameButton_clicked();
+
+    void on_fpsSlider_valueChanged(int value);
+
 private:
     Ui::SpriteMainWindow *ui;
+    QPen pen;
+    QColor penColor;
 };
 
 #endif // SPRITEMAINWINDOW_H
